7a21f85e7700ba31cbd5e477357552ee9e1bc4fd
Kaushik Gopal <code@kaush.co>
2014-05-06 21:18:22 -0700

    test: add test for parcelable ImageDataSource

---
+++ Android-ImageCarouselModule/src/androidTest/java/com/moriahacky/imgcrslapp/imgcrslmodule/ImageDataSourceTest.java
@@ -0,0 +1,31 @@
+package com.moriahacky.imgcrslapp.imgcrslmodule;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.test.AndroidTestCase;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class ImageDataSourceTest
+    extends AndroidTestCase {
+
+    public void test_ThatImageDataSourceIsParcelable() {
+
+        Bundle testBundle = new Bundle();
+        testBundle.putParcelable("imageDataSource1",
+                                 new ImageDataSource(ImageDataSource.ImageType.URL,
+                                                     "https://lh3.googleusercontent.com/-tUnSh4hL1b0/AAAAAAAAAAI/AAAAAAAAjNI/QVV1JIzbIAo/photo.jpg",
+                                                     "Test Title")
+                                );
+
+        Intent testIntent = new Intent("someAction");
+        testIntent.putExtras(testBundle);
+
+        ImageDataSource testSource = testIntent.getParcelableExtra("imageDataSource1");
+
+        assertThat(testSource.getTitle()).isEqualTo("Test Title");
+        assertThat(testSource.getValue()).isEqualTo(
+            "https://lh3.googleusercontent.com/-tUnSh4hL1b0/AAAAAAAAAAI/AAAAAAAAjNI/QVV1JIzbIAo/photo.jpg");
+        assertThat(testSource.getType()).isEqualTo(ImageDataSource.ImageType.URL);
+    }
+}
